4ae6af3108911cb9354b188cbd13ed96a818ac24
danang <danangwijaya750@gmail.com>
2018-11-14 23:33:46 +0700

    create tabs and add osmdroid

--- app/src/main/java/com/dngwjy/app4/activities/MainActivity.java
+++ app/src/main/java/com/dngwjy/app4/activities/MainActivity.java
@@ -1,13 +1,24 @@
 package com.dngwjy.app4.activities;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import com.dngwjy.app4.R;
 import com.dngwjy.app4.data.adapters.MainAdapter;
 import com.dngwjy.app4.data.models.MasjidModel;
+import com.dngwjy.app4.fragments.EventFragment;
+import com.dngwjy.app4.fragments.MapsFragment;
 import com.dngwjy.app4.presenters.MainPresenter;
 import com.dngwjy.app4.utils.SetUpLayMan;
 import com.dngwjy.app4.views.MainView;
@@ -16,24 +27,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MainView {
-private MainPresenter mainPresenter;
-private  RecyclerView recyclerView;
-private MainAdapter adapter;
-private List<MasjidModel> masjidModels= new ArrayList<>();
+private TabLayout tabLayout;
+private ViewPager viewPager;
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainPresenter= new MainPresenter(this);
-        adapter=new MainAdapter(this,masjidModels);
-        recyclerView=(RecyclerView)findViewById(R.id.recMasjid);
-        afterCreate();
+        tabLayout=findViewById(R.id.htab_tabs);
+        viewPager=findViewById(R.id.htab_viewpager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
     }
-    void afterCreate(){
-        recyclerView.setLayoutManager(SetUpLayMan.linearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        mainPresenter.getData();
-    }
+
+    private void setupViewPager(ViewPager viewPage){
+       ViewPagerPagerAdapter adapter= new ViewPagerPagerAdapter(getSupportFragmentManager());
+       adapter.addFragment(new EventFragment(),"Event");
+       adapter.addFragment(new MapsFragment(),"Maps");
+        viewPage.setAdapter(adapter);
+}
 
     @Override
     public void showLoad() {
@@ -47,8 +58,37 @@ private List<MasjidModel> masjidModels= new ArrayList<>();
 
     @Override
     public void showData(List<MasjidModel> data) {
-        masjidModels.clear();
-        masjidModels.addAll(data);
-        adapter.notifyDataSetChanged();
+
+    }
+
+    private class ViewPagerPagerAdapter extends FragmentPagerAdapter {
+    final List<Fragment> fragmentList= new ArrayList<>();
+    final List<String> titleList= new ArrayList<>();
+        public ViewPagerPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragment(Fragment fragment, String title){
+            fragmentList.add(fragment);
+            titleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titleList.get(position);
+        }
     }
 }
